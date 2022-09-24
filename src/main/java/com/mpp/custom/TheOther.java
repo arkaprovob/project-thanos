e55c6e7f3001e1438dc59446448e57d8d9f9e9ad
arbhatta <apb@live.in>
2022-09-24 08:08:17 +0530

    features added and some of the functions are refactored.

--- src/main/java/com/mpp/custom/TheOther.java
+++ src/main/java/com/mpp/custom/TheOther.java
@@ -3,13 +3,19 @@ package com.mpp.custom;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
 import io.fabric8.openshift.api.model.Project;
+import org.javatuples.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class TheOther implements Watcher<Project> {
     private static final Logger LOG = LoggerFactory.getLogger(TheOther.class);
 
     private final Thanos thanos;
+    private static final List<Pair<String, Date>> recentlyDeletedProjects = new ArrayList<>();
 
     public TheOther(Thanos thanos) {
         this.thanos = thanos;
@@ -27,5 +33,14 @@ public class TheOther implements Watcher<Project> {
     @Override
     public void onClose(WatcherException e) {
         LOG.info("onClose event invoked due to {} ", e.asClientException().getStatus().getStatus());
+    }
+
+    public static List<Pair<String, Date>> getRecentlyDeletedProjects() {
+        return recentlyDeletedProjects;
+    }
+
+    public static void setRecentlyDeletedProjects(List<Pair<String, Date>> recentlyDeletedProjects) {
+        TheOther.recentlyDeletedProjects.clear();
+        TheOther.recentlyDeletedProjects.addAll(recentlyDeletedProjects);
     }
 }
